9413e17c43b4b45734aa39577694afa6f4a5b64a
Guilherme Bezerra <guilherme10alves.ga@gmail.com>
2025-12-23 20:48:52 -0300

    Initial commit

---
+++ src/main/java/dev/gbezerra/lms/lms_api/LmsApiApplication.java
@@ -0,0 +1,13 @@
+package dev.gbezerra.lms.lms_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gbezerra/lms/lms_api/LmsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gbezerra.lms.lms_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
